2ec2bbc76a0542cb6d3c3d71d2b1353b10ea795c
Amaral Krichman <amaralkrichman@gmail.com>
2022-11-08 18:45:02 -0300

    Vite config tweak

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,28 @@ import { defineConfig } from "vite";
 const srcDir = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  return {
     plugins: [svelte()],
     resolve: {
-        alias: {
-            src: srcDir,
-            '$features': resolve(srcDir, 'main', 'features'),
-            '$lib': resolve(srcDir, 'main', 'lib'),
-            '$types': resolve(srcDir, 'types'),
-        },
-    },
-    build: {
-      minify: import.meta.env.PROD,
-      rollupOptions: {
-          input: {
-              background: resolve(srcDir, 'background', 'index.ts'),
-              main: resolve(srcDir, 'main', 'index.html'),
-          },
-          output: {
-              entryFileNames: (chunk) => `src/${chunk.name}/index.js`,
-          },
+      alias: {
+        src: srcDir,
+        '$features': resolve(srcDir, 'main', 'features'),
+        '$lib': resolve(srcDir, 'main', 'lib'),
+        '$types': resolve(srcDir, 'types'),
       },
     },
+    build: {
+      minify: false,
+      rollupOptions: {
+        input: {
+          background: resolve(srcDir, 'background', 'index.ts'),
+          main: resolve(srcDir, 'main', 'index.html'),
+        },
+        output: {
+          entryFileNames: (chunk) => `src/${chunk.name}/index.js`,
+        },
+      },
+    },
+  }
 });
