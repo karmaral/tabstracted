09b863a83480f436833fbdceb61886cc136cd7a6
Amaral Krichman <amaralkrichman@gmail.com>
2022-11-10 02:12:17 -0300

    UI exports

--- src/main/features/ui/index.ts
+++ src/main/features/ui/index.ts
@@ -1,7 +1,9 @@
 import EditableTitle from './editable-title.svelte';
+import MenuPopup from './menu/menu-popup.svelte';
 import LoadingWidget from './loading-widget.svelte';
 
 export {
   EditableTitle,
-  LoadingWidget
+  LoadingWidget,
+  MenuPopup,
 };
